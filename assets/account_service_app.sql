8f1b62b18cadd606afdfab2de755ab7bda72a1ea
Muhammad Ilham <ilham9d27@gmail.com>
2023-08-07 09:34:18 +0700

    uploading project model/plan

---
+++ assets/account_service_app.sql
@@ -0,0 +1,35 @@
+-- DDL
+-- create database
+create database db_app_service;
+
+-- use database
+use db_app_service;
+
+-- create table
+create table accounts(
+id varchar(20) not null primary key,
+name varchar(100),
+email varchar(100) not null,
+phone_number varchar(20) not null,
+password varchar(100) not null,
+balance int,
+create_at datetime default current_timestamp,
+update_at datetime,
+delete_at datetime
+);
+
+create table transaction(
+id varchar(20) not null primary key,
+status varchar(10),
+account_id_pengirim varchar(20),
+account_id_penerima varchar(20),
+balance int,
+date_time_transaction datetime,
+constraint fk_account_pengirim foreign key (account_id_pengirim) references accounts(id),
+constraint fk_account_penerima foreign key (account_id_penerima) references accounts(id)
+);
+
+alter table transaction
+drop column status;
+
+select * from transaction;
