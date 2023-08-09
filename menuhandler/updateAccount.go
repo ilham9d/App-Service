8f6fde5f190b6f52d39db46253cf5f2b8bb03f05
Muhammad Ilham <ilham9d27@gmail.com>
2023-08-09 09:12:21 +0700

    feature: update Profile

--- menuhandler/updateAccount.go
+++ menuhandler/updateAccount.go
@@ -4,19 +4,25 @@ import (
 	"app-service/entities"
 	"database/sql"
 	"fmt"
+	"time"
 )
 
 func UbahData(db *sql.DB, query string, user entities.User, before string) {
 	var data string
 	var option string
+	// reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Data Sebelum : ", before)
 	fmt.Print("Masukkan data baru : ")
 	fmt.Scan(&data)
+	// data, _ := reader.ReadString('\n')
 	fmt.Println("Apakah anda yakin ingin mengubahnya?(y/n)")
 	fmt.Scan(&option)
+	// option, _ = reader.ReadString('\n')
 	switch option {
 	case "y", "Y":
-		_, err := db.Exec(query, data, user.Id)
+		var datetime string = time.Now().Format("2006-01-02 15:04:05")
+
+		_, err := db.Exec(query, data, datetime, user.Id)
 		if err != nil {
 			fmt.Println("Update data gagal", err.Error())
 		} else {
@@ -36,19 +42,19 @@ func UpdateAccount(db *sql.DB, user entities.User) {
 
 	switch pilihan {
 	case 1:
-		query := "Update accounts set name = ? where id = ?"
+		query := "Update accounts set name = ?, update_at = ? where id = ?"
 		before := user.Nama
 		UbahData(db, query, user, before)
 	case 2:
-		query := "Update accounts set email = ? where id = ?"
+		query := "Update accounts set email = ?, update_at = ? where id = ?"
 		before := user.Email
 		UbahData(db, query, user, before)
 	case 3:
-		query := "Update accounts set phone_number = ? where id = ?"
+		query := "Update accounts set phone_number = ?, update_at = ? where id = ?"
 		before := user.PhoneNumber
 		UbahData(db, query, user, before)
 	case 4:
-		query := "Update accounts set password = ? where id = ?"
+		query := "Update accounts set password = ?, update_at = ? where id = ?"
 		before := user.Password
 		UbahData(db, query, user, before)
 	default:
