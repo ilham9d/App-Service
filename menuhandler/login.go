d2f7991e2991efc004b2e994577f1c368fce912e
Muhammad Ilham <ilham9d27@gmail.com>
2023-08-09 20:26:32 +0700

    modified menuhanler/login

--- menuhandler/login.go
+++ menuhandler/login.go
@@ -4,7 +4,6 @@ import (
 	"app-service/entities"
 	"database/sql"
 	"fmt"
-	"log"
 )
 
 func Login(db *sql.DB) {
@@ -19,24 +18,25 @@ func Login(db *sql.DB) {
 
 	row := db.QueryRow("select id, name, email, phone_number, password, balance, create_at, update_at, delete_at from accounts where phone_number = ? and delete_at is null", telp)
 	if err := row.Scan(&user.Id, &user.Nama, &user.Email, &user.PhoneNumber, &user.Password, &user.Balance, &create, &update, &delete); err != nil {
-		log.Fatal("error scan data ", err.Error())
-	}
-	if create.Valid {
-		user.Create_at = create.String
-	}
-	if update.Valid {
-		user.Update_at = update.String
-	}
-	if delete.Valid {
-		user.Delete_at = delete.String
-	}
-
-	fmt.Println("phone:", user.PhoneNumber)
-	fmt.Println("pass: ", user.Password)
-	if telp == user.PhoneNumber && pass == user.Password {
-		fmt.Println("login berhasil")
-		MenuUtama(db, user) //memanggil menu utama
+		fmt.Println("Akun tidak terdaftar ", err.Error())
 	} else {
-		fmt.Println("no telpon atau password salah")
+		if create.Valid {
+			user.Create_at = create.String
+		}
+		if update.Valid {
+			user.Update_at = update.String
+		}
+		if delete.Valid {
+			user.Delete_at = delete.String
+		}
+
+		// fmt.Println("phone:", user.PhoneNumber)
+		// fmt.Println("pass: ", user.Password)
+		if telp == user.PhoneNumber && pass == user.Password {
+			fmt.Println("login berhasil!")
+			MenuUtama(db, user) //memanggil menu utama
+		} else {
+			fmt.Println("no telpon atau password salah.")
+		}
 	}
 }
