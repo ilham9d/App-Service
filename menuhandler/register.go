bf5599408777f78ce226b721ad00a9f13f2fe774
Muhammad Ilham <ilham9d27@gmail.com>
2023-08-09 21:20:51 +0700

    modified feature register

--- menuhandler/register.go
+++ menuhandler/register.go
@@ -7,40 +7,54 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
 func Register(db *sql.DB) {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Insert Your ID: ")
-	ID, _ := reader.ReadString('\n')
-	ID = strings.TrimSpace(ID)
+	var str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	newid, err := gonanoid.Generate(str, 20)
+	if err != nil {
+		log.Default()
+	}
 
-	fmt.Print("Insert Your Name: ")
+	fmt.Print("Masukkan nama anda: ")
 	Name, _ := reader.ReadString('\n')
 	Name = strings.TrimSpace(Name)
 
-	fmt.Print("Insert Your Password: ")
+	fmt.Print("Masukkan password anda: ")
 	Password, _ := reader.ReadString('\n')
 	Password = strings.TrimSpace(Password)
 
-	fmt.Print("Insert Your Email: ")
+	fmt.Print("Masukkan email anda: ")
 	Email, _ := reader.ReadString('\n')
 	Email = strings.TrimSpace(Email)
 
-	fmt.Print("Insert Your Phone Number: ")
+	fmt.Print("Masukkan nomor telpon anda :")
 	Phone_Number, _ := reader.ReadString('\n')
 	Phone_Number = strings.TrimSpace(Phone_Number)
 
-	result, errInsert := db.Exec("INSERT INTO accounts (id, name, password, email, phone_number) VALUES (?, ?, ?, ?, ?)", ID, Name, Password, Email, Phone_Number)
-	if errInsert != nil {
-		log.Fatal("error insert", errInsert.Error())
-	} else {
-		row, _ := result.RowsAffected()
-		if row > 0 {
-			fmt.Println("success to register")
+	var option string
+	fmt.Println("Apakah data anda sudah benar?(y/n)")
+	fmt.Print("Masukkan pilihan anda: ")
+	fmt.Scanln(&option)
+	switch option {
+	case "y", "Y":
+		result, errInsert := db.Exec("INSERT INTO accounts (id, name, password, email, phone_number) VALUES (?, ?, ?, ?, ?)", newid, Name, Password, Email, Phone_Number)
+		if errInsert != nil {
+			log.Fatal("error insert", errInsert.Error())
 		} else {
-			fmt.Println("failed to register")
+			row, _ := result.RowsAffected()
+			if row > 0 {
+				fmt.Println("Berhasil mendaftar")
+			} else {
+				fmt.Println("Gagal mendaftar")
+			}
 		}
+	case "n", "N":
+		fmt.Println("Batal mendaftar")
 	}
+
 }
